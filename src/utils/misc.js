d49a9bbae6f86189c09ac70239498c853883f9b3
Adas <m21032622@gmail.com>
2024-07-10 22:40:38 +0530

    v0.0.2

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,4 +1,31 @@
 /**
+Check if string is a positive integer then convert it.
+**/
+export function isPositiveIntegerString(str) {
+    const integerRegex = /^\d+$/;
+
+    if (integerRegex.test(str)) {
+        return parseInt(str, 10);
+    }
+
+    return null;
+}
+
+/**
+Generate url safe token.
+**/
+export function generateUrlSafeToken(len) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
+    let result = '';
+
+    for (let i = 0; i < len; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return result
+}
+
+/**
 Generate random file name.
 **/
 export function generateRandomFileName(
