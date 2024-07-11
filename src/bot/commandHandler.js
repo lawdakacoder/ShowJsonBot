925d0859ff5b210caa184a660a48109e87b729c7
Adas <m21032622@gmail.com>
2024-07-11 12:49:21 +0530

    v0.0.3

--- src/bot/commandHandler.js
+++ src/bot/commandHandler.js
@@ -2,50 +2,82 @@ import { sendMessage, sendJsonMessage } from "../telegram/api";
 import { Texts } from "../utils/static";
 import { isPositiveIntegerString, generateUrlSafeToken } from "../utils/misc";
 
-export async function handleCommand(message) {
-    const { from: user, chat, text } = message;
-    const [command, ...args] = text.split(' ');
+async function startCommand(message) {
+    const { from: user, chat} = message;
+    const response = Texts.start
+    .replace('{first_name}', user.first_name);
+    const reply_markup = {
+        inline_keyboard: [[{text: 'Source Code', url: 'https://github.com/lawdakacoder/ShowJsonBot'}]]
+    };
+
+    await sendMessage(chat.id, response, message.message_id, reply_markup);
+    return true;
+}
+
+async function helpCommand(message) {
+    const { from: user, chat} = message;
+    const response = Texts.help;
+
+    await sendMessage(chat.id, response, message.message_id);
+    return true;
+}
+
+async function infoCommand(message) {
+    const { from: user, chat} = message;
+    const response = JSON.stringify(user, null, 2);
+
+    await sendJsonMessage(chat.id, response, message.message_id);
+    return true;
+}
+
+async function chatCommand(message) {
+    const { chat } = message;
+    const response = JSON.stringify(chat, null, 2);
+
+    await sendJsonMessage(chat.id, response, message.message_id);
+    return true;
+}
+
+async function secretCommand(message, args) {
+    const { chat } = message;
     let response = '';
+    let reply_markup = {};
+
+    if (args.length === 0) {
+        response = Texts.secret;
+    } else {
+        const num = isPositiveIntegerString(args[0]);
+
+        if (num === null || num === 0) {
+            response = Texts.notValidArgument;
+        } else if (num > 4096) {
+            response = Texts.expectedValueTooBig;
+        } else {
+            response = `<code>${generateUrlSafeToken(num)}</code>`;
+            reply_markup.inline_keyboard = 
+            [[{text: 'Refresh Token', callback_data: 'refresh_secret_token'}]];
+        }
+    }
+
+    await sendMessage(chat.id, response, message.message_id, reply_markup);
+    return true;
+}
+
+export async function handleCommand(message) {
+    const { text } = message;
+    const [command, ...args] = text.split(' ');
 
     switch (command) {
         case '/start':
-            response = Texts.start
-            .replace('{first_name}', user.first_name);
-
-            const reply_markup = {
-                inline_keyboard: [[{ text: 'Source Code', url: 'https://github.com/lawdakacoder/ShowJsonBot'}]]
-            };
-            
-            await sendMessage(chat.id, response, message.message_id, reply_markup);
-            return true;
+            return await startCommand(message)
         case '/help':
-            response = Texts.help;
-            await sendMessage(chat.id, response, message.message_id);
-            return true;
+            return await helpCommand(message)
         case '/info':
-            response = JSON.stringify(user, null, 2);
-            await sendJsonMessage(chat.id, response, message.message_id);
-            return true;
+            return await infoCommand(message)
         case '/chat':
-            response = JSON.stringify(chat, null, 2);
-            await sendJsonMessage(chat.id, response, message.message_id);
-            return true;
+            return await chatCommand(message)
         case '/secret':
-            if (args.length === 0) {
-                response = Texts.secret;
-            } else {
-                const num = isPositiveIntegerString(args[0]);
-
-                if (num === null) {
-                    response = Texts.notValidArgument;
-                } else if (num > 4096) {
-                    response = Texts.expectedValueTooBig;
-                } else {
-                    response = `<code>${generateUrlSafeToken(num)}</code>`;
-                }
-            }
-            await sendMessage(chat.id, response, message.message_id);
-            return true;
+            return await secretCommand(message, args)
         default:
             return false;
     }
